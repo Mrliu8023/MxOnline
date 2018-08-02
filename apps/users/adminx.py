1b484b4b989d63754c0072e223b7c487c2290aa3
xijue <mrliuxiansen8023@gmail.com>
2018-08-02 16:05:06 +0800

    全局配置xadmin，添加主题以及更改左侧导航样式

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -4,9 +4,21 @@ __date__ = '18-8-2 上午11:03'
 
 
 import xadmin
+from xadmin import views
 
 from models import EmailVerifyRecord
 from models import Banner
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobaSettings(object):
+    site_title = "佩奇后台管理系统"
+    site_footer = "佩奇教育"
+    menu_style = "accordion"
 
 
 class EmailVerifyRecordAdmin(object):
@@ -25,3 +37,8 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
 xadmin.site.register(Banner,BannerAdmin)
+
+#注册主题功能
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+
+xadmin.site.register(views.CommAdminView, GlobaSettings)
