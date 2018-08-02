ffb61d88a9766151a5fb417c981bd733c8f3c800
xijue <mrliuxiansen8023@gmail.com>
2018-08-02 12:50:06 +0800

    对model进行了一些修改

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -11,7 +11,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50,verbose_name=u"课程名称")
     desc = models.CharField(max_length=300,verbose_name=u"课程描述")
     detail = models.TextField(verbose_name=u"课程详情")
-    degree = models.CharField(choices=(("cj","初级"),("zj","中级"),("gj","高级")),max_length=2)
+    degree = models.CharField(verbose_name=u"课程难度", choices=(("cj","初级"),("zj","中级"),("gj","高级")),max_length=2)
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
     students = models.IntegerField(default=0,verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0,verbose_name=u"收藏人数")
@@ -23,14 +23,18 @@ class Course(models.Model):
         verbose_name = u"课程"
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course,verbose_name=u"课程")
     name = models.CharField(max_length=100,verbose_name=u"章节名")
     add_time = models.DateTimeField(default=datetime.now,verbose_name=u"添加时间")
 
-    verbose_name = u"章节"
-    verbose_name_plural = verbose_name
+    class Meta:
+        verbose_name = u"章节"
+        verbose_name_plural = verbose_name
 
 
 class Video(models.Model):
@@ -38,8 +42,10 @@ class Video(models.Model):
     lesson = models.ForeignKey(Lesson,verbose_name=u"章节")
     add_time = models.DateTimeField(default=datetime.now,verbose_name=u"添加时间")
 
-    verbose_name = u"视频"
-    verbose_name_plural = verbose_name
+    class Meta:
+        verbose_name = u"视频"
+        verbose_name_plural = verbose_name
+
 
 class ConrseResourse(models.Model):
     course = models.ForeignKey(Course,verbose_name=u"课程")
--- apps/organization/models.py
+++ apps/organization/models.py
@@ -37,7 +37,6 @@ class CourseOrg(models.Model):
 class Teacher(models.Model):
     org = models.ForeignKey(CourseOrg, verbose_name=u"所属机构")
     name = models.CharField(max_length=50, verbose_name=u"教师名")
-    name = models.CharField(max_length=50, verbose_name=u"教师名")
     work_years = models.IntegerField(default=0, verbose_name=u"工作年限")
     work_company = models.CharField(max_length=50, verbose_name=u"就职公司")
     work_position = models.CharField(max_length=50, verbose_name=u"公司职位")
@@ -47,7 +46,6 @@ class Teacher(models.Model):
     # age = models.IntegerField(default=18, verbose_name=u"年龄")
     # image = models.ImageField(default='', upload_to="teacher/%Y/%m", verbose_name = u"头像", max_length = 100)
     add_time = models.DateTimeField(default=datetime.now)
-
 
     class Meta:
         verbose_name = u"教师"
