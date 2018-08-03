bff1e130c947be6c61899f282b10d4f2e23e8a94
xijue <mrliuxiansen8023@gmail.com>
2018-08-03 09:45:49 +0800

    通过类来实现视图

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
 
@@ -22,17 +23,33 @@ class CustomBackend(ModelBackend):
     pass
 
 
-def user_login(request):
-    if request.method == "POST":
+class LoginView(View):
+    #用类来实现视图
+    def get(self,request):
+        return render(request, "login.html", {})
 
-        user_name = request.POST.get("username","")
-        pass_word = request.POST.get("password","")
-        user = authenticate(username = user_name,password = pass_word)
+    def post(self,request):
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(username=user_name, password=pass_word)
         if user is not None:
-            login(request,user)
+            login(request, user)
             return render(request, "index.html")
         else:
-            return render(request, "login.html", {"msg":"用户名或密码错误！"})
+            return render(request, "login.html", {"msg": "用户名或密码错误！"})
 
-    elif request.method == "GET":
-        return render(request,"login.html",{})
+
+# def user_login(request):
+#     if request.method == "POST":
+#
+#         user_name = request.POST.get("username","")
+#         pass_word = request.POST.get("password","")
+#         user = authenticate(username = user_name,password = pass_word)
+#         if user is not None:
+#             login(request,user)
+#             return render(request, "index.html")
+#         else:
+#             return render(request, "login.html", {"msg":"用户名或密码错误！"})
+#
+#     elif request.method == "GET":
+#         return render(request,"login.html",{})
